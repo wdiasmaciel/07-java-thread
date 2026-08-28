297fb01b3380279a03f3e535caf80e33182a1f9a
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-28 20:48:50 +0000

    Exemplo

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // Exemplo de Mutex:
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria();
+        ContaBancaria conta = new ContaBancaria(100);
 
         // Criamos duas threads tentando sacar R$70 ao mesmo tempo do saldo de R$100:
         Thread t1 = new Thread(() -> conta.sacar("Thread_A", 70));
